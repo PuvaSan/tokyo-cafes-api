9304612143ac993e71c5f70ac067cd2554ebebad
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-10-29 14:14:25 +0900

    seed successful

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "Grabbing Cafes"
 url = 'https://gist.githubusercontent.com/yannklein/5d8f9acb1c22549a4ede848712ed651a/raw/3daec24bcd833f0dd3bcc8cee8616a731afd1f37/cafe.json'
 
 # http request to get JSON data from gh
-json_cafes = JSON.parse(open(url).read)
+json_cafes = URI.open(url).read
 
 # convert to ruby object
 cafes = JSON.parse(json_cafes)
