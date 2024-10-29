ce31aaf9657fa3e8c9090e98fb35aea8a4a9d3bc
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-10-29 14:41:44 +0900

    created the index method for all by returning JSON

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::CafesController < ApplicationController
+  def index
+    if params[:title].present?
+      @cafes = Cafe.where('title ILIKE ?', "%#{params[:title]}%")
+    else
+      @cafes = Cafe.all
+    end
+
+    # return json in descending order
+    render json: @cafes.order(create_at: :desc)
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
 end
