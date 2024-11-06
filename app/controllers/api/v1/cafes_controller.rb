50956f36d6ae7ce93bd5a26c84b1fa12c7a61ead
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-11-06 15:39:25 +0900

    create method and params methods set

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -9,14 +9,19 @@ class Api::V1::CafesController < ApplicationController
     @cafes = params[:title].present? ? Cafe.where('title ILIKE ?', "%#{params[:title]}%") : Cafe.all
 
     # return json in descending order
-    render json: @cafes
+    render json: @cafes.order(created_at: :desc)
   end
 
-  # def create
-
-  # end
+  def create
+    @cafe = Cafe.new(cafe_params)
+  end
 
   # def update
 
   # end
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:title, :address, :picture, hours: {}, criteria: [])
+  end
 end
