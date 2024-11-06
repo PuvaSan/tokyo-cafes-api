bad8e72cfbd76de372036f69f364572059c5f1cf
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-11-06 11:45:39 +0900

    title search refactored into ternary

--- app/controllers/api/v1/cafes_controller.rb
+++ app/controllers/api/v1/cafes_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::CafesController < ApplicationController
   def index
-    if params[:title].present?
-      @cafes = Cafe.where('title ILIKE ?', "%#{params[:title]}%")
-    else
-      @cafes = Cafe.all
-    end
+    # if params[:title].present?
+    #   @cafes = Cafe.where('title ILIKE ?', "%#{params[:title]}%")
+    # else
+    #   @cafes = Cafe.all
+    # end
+
+    @cafes = params[:title].present? ? Cafe.where('title ILIKE ?', "%#{params[:title]}%") : Cafe.all
 
     # return json in descending order
     render json: @cafes
